1d0b9d8c270ff6b8aff944a00e09e7f069d2fe7a
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-11-29 22:32:55 -0600

    added pagination component

--- BlazorMovie/Services/TMDBClient.cs
+++ BlazorMovie/Services/TMDBClient.cs
@@ -11,17 +11,20 @@ namespace BlazorMovie.Services
         public TMDBClient(HttpClient httpClient, IConfiguration config)
         {
             _httpClient = httpClient;
-            
+
             _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
-            _httpClient.DefaultRequestHeaders.Accept.Add(new( "application/json"));
+            _httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
 
             string apiKey = config["TMDBKey"] ?? throw new Exception("TMDBKey not found.");
-            _httpClient.DefaultRequestHeaders.Authorization= new("Bearer", apiKey);
+            _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
         }
 
-       public Task<PopularMoviesPageResponse?> GetPopularMoviesAsync()
+        public Task<PopularMoviesPageResponse?> GetPopularMoviesAsync(int page = 1)
         {
-            return _httpClient.GetFromJsonAsync<PopularMoviesPageResponse>("movie/popular");
+            if (page < 1) page = 1;
+            if (page > 500) page = 500;
+
+            return _httpClient.GetFromJsonAsync<PopularMoviesPageResponse>($"movie/popular?page={page}");
         }
         public Task<MovieDetails?> GetMovieDetailsAsync(int id)
         {
