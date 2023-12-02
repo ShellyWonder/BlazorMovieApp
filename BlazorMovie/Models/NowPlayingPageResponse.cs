c1df11f8f4b9983f0894b7cec87051c6e842f15e
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-12-01 23:11:36 -0600

    creating models for NowPlaying API category

---
+++ BlazorMovie/Models/NowPlayingPageResponse.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovie.Models
+{
+    public class NowPlayingPageResponse
+    {
+
+        [JsonPropertyName("dates")]
+        public Dates? Dates { get; set; }
+
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("results")]
+        public IEnumerable<NowPlaying> Results { get; set; } = [];
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonPropertyName("total_results")]
+        public int TotalResults { get; set; }
+    }
+
+
+
+
+}
