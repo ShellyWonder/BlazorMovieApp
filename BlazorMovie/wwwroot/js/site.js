6656439040c743f60f6875ed0e863f9ef8ceab94
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-23 22:20:48 -0600

    Implemented sessionStorage in PopularMovies component.

--- BlazorMovie/wwwroot/js/site.js
+++ BlazorMovie/wwwroot/js/site.js
@@ -1,3 +1,19 @@
+//accessing sessionStorage
+if (window.sessionStorageHelper) {
+    window.sessionStorageHelper = {
+        setItem: function (key, value) {
+            sessionStorage.setItem(key, JSON.stringify(value));
+        },
+        getItem: function (key) {
+            const item = sessionStorage.getItem(key);
+            return item ? JSON.parse(item) : null;
+        }
+    }
+} else {
+    console.error("sessionStorageHelper is not initialized");
+}
+
+
 function showModal(modalId) {
     $('#' + modalId).modal('show');
 }
