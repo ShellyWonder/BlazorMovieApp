2d408836a2ff15ded0f6a0d82349c2663b280c0d
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-11-30 01:22:18 -0600

    Modified models to inherit from parent class, movie.cs

--- BlazorMovie/Models/Movie.cs
+++ BlazorMovie/Models/Movie.cs
@@ -10,6 +10,10 @@ namespace BlazorMovie.Models
         [JsonPropertyName("title")]
         public string? Title { get; set; }
 
+        [JsonPropertyName("adult")]
+        public bool Adult { get; set; }
+
+
         [JsonPropertyName("poster_path")]
         public string? PosterPath { get; set; }
 
@@ -19,7 +23,29 @@ namespace BlazorMovie.Models
         [JsonPropertyName("popularity")]
         public float Popularity { get; set; }
 
+        [JsonPropertyName("overview")]
+        public string Overview { get; set; } = String.Empty;
+
         [JsonPropertyName("backdrop_path")]
         public string BackdropPath { get; set; } = string.Empty;
+
+        [JsonPropertyName("vote_average")]
+        public float VoteAverage { get; set; }
+
+        [JsonPropertyName("original_title")]
+        public string OriginalTitle { get; set; } = String.Empty;
+
+        [JsonPropertyName("original_language")]
+        public string OriginalLanguage { get; set; } = String.Empty;
+        
+        [JsonPropertyName("genre_ids")]
+        public List<int> GenreIds { get; set; } = new();
+
+
+        [JsonPropertyName("vote_count")]
+        public int VoteCount { get; set; }
+
+        [JsonPropertyName("video")]
+        public bool Video { get; set; }
     }
 }
