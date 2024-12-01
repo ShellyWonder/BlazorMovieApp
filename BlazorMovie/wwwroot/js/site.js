a544cd4c6a96e2f89f5d0eff603ca2bbe8f30dba
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-12-01 09:31:46 -0600

    sessionStorage tweaking

--- BlazorMovie/wwwroot/js/site.js
+++ BlazorMovie/wwwroot/js/site.js
@@ -1,18 +1,12 @@
-//accessing sessionStorage
-
 window.sessionStorageHelper = {
     setItem: function (key, value) {
         sessionStorage.setItem(key, JSON.stringify(value));
     },
-
     getItem: function (key) {
-        // Return raw JSON string for debugging purposes
-        return sessionStorage.getItem(key);
-        console.log(sessionStorage.getItem(key));
+        const value = sessionStorage.getItem(key);
+        return value ? value : null;
     }
 };
-
-
 
 function showModal(modalId) {
     $('#' + modalId).modal('show');
