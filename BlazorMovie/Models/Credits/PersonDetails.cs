53e7d2ce9d2201bd593f9fe074b1e23a993f3251
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-12-12 16:46:18 -0600

    Added PersonDetails;

---
+++ BlazorMovie/Models/Credits/PersonDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovie.Models.Credits
+{
+    public class PersonDetails : Person
+    {
+        [JsonPropertyName("also_known_as")]
+        public string[] AKA { get; set; } = [];
+
+        [JsonPropertyName("biography")]
+        public string Biography { get; set; } = string.Empty;
+        
+        [JsonPropertyName("birthday")]
+        public string Birthdate { get; set; }
+
+        [JsonPropertyName("deathday")]
+        public object Deathdate { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public object Homepage { get; set; } = string.Empty;
+
+        [JsonPropertyName("imdb_id")]
+        public string ImdbId { get; set; } = string.Empty;
+
+        [JsonPropertyName("place_of_birth")]
+        public string Birthplace { get; set; } = string.Empty;
+           
+    }
+
+    
+}
