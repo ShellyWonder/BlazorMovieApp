caa8ecd805ba7db799faeeec2c13bb8b22eed27d
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-12-17 07:36:20 -0600

    modified PersonDetails

--- BlazorMovie/Models/Credits/PersonDetails.cs
+++ BlazorMovie/Models/Credits/PersonDetails.cs
@@ -5,21 +5,21 @@ namespace BlazorMovie.Models.Credits
     public class PersonDetails : Person
     {
         [JsonPropertyName("also_known_as")]
-        public string[] AKA { get; set; } = [];
+        public string[]? AKA { get; set; } 
 
         [JsonPropertyName("biography")]
         public string Biography { get; set; } = string.Empty;
         
         [JsonPropertyName("birthday")]
-        public string Birthdate { get; set; }
+        public DateOnly? Birthdate { get; set; }
 
         [JsonPropertyName("deathday")]
-        public object Deathdate { get; set; }
+        public DateOnly? Deathdate { get; set; }
 
         [JsonPropertyName("homepage")]
-        public object Homepage { get; set; } = string.Empty;
+        public object Homepage { get; set; } = string.Empty; //Use ValidateLinkComponent
 
-        [JsonPropertyName("imdb_id")]
+        [JsonPropertyName("imdb_id")] //Use ValidateLinkComponent
         public string ImdbId { get; set; } = string.Empty;
 
         [JsonPropertyName("place_of_birth")]
