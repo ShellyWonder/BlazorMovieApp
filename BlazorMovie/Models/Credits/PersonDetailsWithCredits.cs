a4bbf9ce9ed5f13e0e2fca069fc67383086cf746
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-02-15 14:49:31 -0600

    Added new class

---
+++ BlazorMovie/Models/Credits/PersonDetailsWithCredits.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovie.Models.Credits
+{
+    public class PersonDetailsWithCredits : PersonDetails
+    {
+        [JsonPropertyName("movie_credits")]
+        public  Credit? MovieCredits { get; set; }
+    }
+}
