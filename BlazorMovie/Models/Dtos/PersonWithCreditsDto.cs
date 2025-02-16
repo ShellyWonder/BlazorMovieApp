2f9824de2f0704e5f3d50990839ac2d40f5908d8
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-02-16 00:18:05 -0600

    added models to implement new method in TMDBClient.cs

---
+++ BlazorMovie/Models/Dtos/PersonWithCreditsDto.cs
@@ -0,0 +1,10 @@
+﻿using BlazorMovie.Models.Credits;
+namespace BlazorMovie.Models.Dtos
+{
+
+    public class PersonWithCreditsDto
+    {
+        public PersonDetails PersonDetails { get; set; } = new PersonDetails();
+        public PageResponse<MovieWithCharacter> MovieCredits { get; set; } = new PageResponse<MovieWithCharacter>();
+    }
+}
