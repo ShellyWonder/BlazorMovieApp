721884a85954ebb90a45fa501c8cadb97746816a
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-02-17 13:34:56 -0600

    Replaced MovieWithCharacter.cs with MovieCastDto.cs; More streamlined.

--- BlazorMovie/Models/Dtos/MovieCastDto.cs
+++ BlazorMovie/Models/Dtos/MovieCastDto.cs
@@ -2,7 +2,7 @@
 
 namespace BlazorMovie.Models.Dtos
 {
-    public class MovieCastDto
+    public class MovieCastDto : Movie
     {
         [JsonPropertyName("cast_id")]
         public int CastId { get; set; }
@@ -13,20 +13,8 @@ namespace BlazorMovie.Models.Dtos
         [JsonPropertyName("order")]
         public int Order { get; set; }
 
-        // Fields related to the movie
         [JsonPropertyName("id")]
         public int MovieId { get; set; }
-
-        [JsonPropertyName("title")]
-        public string? Title { get; set; }
-
-        [JsonPropertyName("poster_path")]
-        public string? PosterPath { get; set; }
-
-        [JsonPropertyName("release_date")]
-        public string? ReleaseDate { get; set; }
-
-        [JsonPropertyName("vote_average")]
-        public float VoteAverage { get; set; }
+ 
     }
 }
