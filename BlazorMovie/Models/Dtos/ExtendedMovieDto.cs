ef81e79cde2cebb0096ef83e8c11694a68eb7f10
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-02-21 12:44:02 -0600

    Created model to access character property.

---
+++ BlazorMovie/Models/Dtos/ExtendedMovieDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorMovie.Models.Dtos
+{
+    public class ExtendedMovieDto : Movie
+    {
+        [JsonPropertyName("character")]
+        public string? Character { get; set; }
+    }
+}
