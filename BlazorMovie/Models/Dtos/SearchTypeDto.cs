0c2fb771368f81d97a9e6870244295ab1c2da6a2
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2025-02-27 13:48:46 -0600

    Re-implemented SearchTypeDto

---
+++ BlazorMovie/Models/Dtos/SearchTypeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorMovie.Models.Dtos
+{
+    public class SearchTypeDto
+    {
+        public string Movie { get; set; } = string.Empty;
+        public string Person { get; set; } = string.Empty;
+
+        //implementing Genre search in next feature update
+        public string Genre { get; set; } = string.Empty;
+    }
+}
